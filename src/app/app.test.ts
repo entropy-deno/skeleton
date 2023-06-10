6697d8c595d18688e46aa866d44943b09f1905c6
dominiq007 <dom.rajkowski@gmail.com>
2023-06-10 15:26:04 +0200

    test: add app module test

---
+++ src/app/app.test.ts
@@ -0,0 +1,11 @@
+import { assertEquals } from '@std/testing/asserts.ts';
+import { inject } from '@flavor';
+import { AppController } from './app.controller.ts';
+
+Deno.test('app module', async (test) => {
+  await test.step('controller returns a proper home page', () => {
+    const response = inject(AppController).index();
+
+    assertEquals(response, 'Hello World!');
+  });
+});
