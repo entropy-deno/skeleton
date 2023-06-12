022278674e90b1962c30e7511cca5ee0a9b778c2
dominiq007 <dom.rajkowski@gmail.com>
2023-06-12 21:40:30 +0200

    feat: use new routing and config api

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,4 +1,14 @@
-export class AppController {
+import { Controller, HttpMethod } from '@entropy';
+
+export class AppController implements Controller<AppController> {
+  public routes = [
+    {
+      path: '/' as const,
+      action: 'index' as const,
+      methods: [HttpMethod.Get],
+    },
+  ];
+
   public index(): string {
     return 'Hello World!';
   }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app/app.module.ts';
 
 if (import.meta.main) {
   const server = createServer({
+    config: {
+      envFile: '.env',
+    },
     modules: [
       AppModule,
     ],
