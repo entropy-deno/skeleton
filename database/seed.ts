94642a0062f1aa531c86b2dc414cb14853ae68af
dominiq007 <dom.rajkowski@gmail.com>
2023-06-14 19:32:55 +0200

    feat: add db seeder

---
+++ database/seed.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from './client/deno/edge.ts';
+import { Encrypter, inject } from '@entropy';
+
+const db = inject(PrismaClient);
+const encrypter = inject(Encrypter);
+
+await db.user.create({
+  data: {
+    name: 'Admin',
+    email: 'admin@entropy-deno.com',
+    password: encrypter.hash('1234'),
+  },
+});
+
+await db.$disconnect();
