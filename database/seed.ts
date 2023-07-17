894c610ecf54b7e169f6499cf4696e527225d97f
dominiq007 <dom.rajkowski@gmail.com>
2023-07-17 21:19:20 +0200

    chore: update db import mappings

--- database/seed.ts
+++ database/seed.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from './client/deno/edge.ts';
 import { Encrypter, inject } from '@entropy';
+import { PrismaClient } from '@prisma';
 
 const db = inject(PrismaClient);
 const encrypter = inject(Encrypter);
@@ -8,7 +8,7 @@ await db.user.create({
   data: {
     name: 'Admin',
     email: 'admin@entropy.deno.dev',
-    password: encrypter.hash('1234'),
+    password: await encrypter.hash('1234'),
   },
 });
 
