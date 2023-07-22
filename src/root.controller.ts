52004dbc65a35dab949b5dd95ee20cdd9f9691a1
dominiq007 <dom.rajkowski@gmail.com>
2023-07-22 21:57:25 +0200

    feat: sync with api changes

--- src/root.controller.ts
+++ src/root.controller.ts
@@ -2,14 +2,14 @@ import { Controller, render, Route } from '@entropy';
 
 export class RootController implements Controller {
   @Route.Get('/')
-  public async index() {
-    return await render('home', {
+  public index() {
+    return render('home', {
       message: 'Hello, world!',
     });
   }
 
   @Route.Error(404)
-  public async notFound() {
-    return await render('404');
+  public notFound() {
+    return render('404');
   }
 }
