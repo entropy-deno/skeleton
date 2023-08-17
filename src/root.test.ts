02697b0aed55fbd0ad3b9c36f769af76169e56c9
dominiq007 <dom.rajkowski@gmail.com>
2023-08-17 18:27:42 +0200

    test: sync with api changes

--- src/root.test.ts
+++ src/root.test.ts
@@ -1,17 +1,11 @@
 import { assertStringIncludes } from '@std/assert/mod.ts';
-import { inject } from '@entropy/injector';
-import { Router } from '@entropy/router';
+import { fetchRoute } from '@entropy/testing';
 import { RootController } from './root.controller.ts';
 
 Deno.test('root module', async (test) => {
-  const router = inject(Router);
+  await test.step('root controller returns a proper home page', async () => {
+    const responseContent = await fetchRoute('/', RootController);
 
-  await test.step('controller returns a proper home page', async () => {
-    router.registerController(RootController);
-
-    const request = new Request('http://localhost:5050/');
-    const response = await router.respond(request);
-
-    assertStringIncludes(await response.text(), 'Hello, world!');
+    assertStringIncludes(responseContent, 'Hello, world!');
   });
 });
