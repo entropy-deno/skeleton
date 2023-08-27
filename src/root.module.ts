bee86a427d4e0b021c5041073310f7e8b00a0fa2
dominiq007 <dom.rajkowski@gmail.com>
2023-08-27 18:49:28 +0200

    feat: add empty channel array

--- src/root.module.ts
+++ src/root.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@entropy/server';
 import { RootController } from './root.controller.ts';
 
 export class RootModule implements Module {
+  public readonly channels = [];
+
   public readonly controllers = [
     RootController,
   ];
