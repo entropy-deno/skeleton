92d8541d24efb3dbfe00187f1dba81789e46756c
dominiq007 <dom.rajkowski@gmail.com>
2023-11-26 15:52:53 +0100

    refactor: replace using with const

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createServer } from '@entropy/server';
 import { RootModule } from './root.module.ts';
 
 if (import.meta.main) {
-  using server = createServer({
+  const server = createServer({
     config: {
       contentSecurityPolicy: {
         allowedOrigins: [
