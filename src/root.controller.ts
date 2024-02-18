cc9605abc14824cc2b97feebaf8b63727e01ab47
dominiq007 <dom.rajkowski@gmail.com>
2024-02-18 16:25:50 +0100

    feat: redesign

--- src/root.controller.ts
+++ src/root.controller.ts
@@ -5,7 +5,7 @@ export class RootController extends Controller {
   @Route.Get('/')
   public async index() {
     return await this.render('home', {
-      message: 'Hello, world!',
+      message: 'Welcome to Entropy',
     });
   }
 
